57bf1aefabd34b9592c6f769bbe4f4ccee3eee60
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2014-09-25 14:49:23 +0200

    CI

---
+++ Dropbox-jamoma.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+### Script to automate dropbox deployment on the CI machine ###
+###### Build using all the cores ######
+ARCH=`uname -m`
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	DISTRO=$(cat /etc/lsb-release | grep DISTRIB_ID | cut -f2 -d=)
+	DISTROVERSION=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -f2 -d=)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	DISTRO="OS X"
+fi
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	FOLDER="/home/ossia/Dropbox/Iscore/Releases/$DISTRO$DISTROVERSION/$ARCH"
+	cp build/jamoma/Jamomacore-0.6-dev-Linux.deb $FOLDER
+fi
