c050d2291e796fbcbdbd1fc9f8b27e84f1e28af1
Antoine Villeret <antoine.villeret@gmail.com>
2014-11-11 23:24:43 +0100

    add documentation about what ./Update.sh is for

--- Update.sh
+++ Update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# This script is intended to update all the sub-repos to build a cutting edge i-score with the latest sources available.
 (cd Jamoma/Core; git pull)
 (cd Jamoma/Core/Score; git pull)
 (cd i-score; git pull)
