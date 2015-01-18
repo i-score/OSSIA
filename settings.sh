990660533965d6e2bb87b015d710fc2eedbdb388
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-01-19 00:15:28 +0100

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,3 +1,3 @@
 # Put here the path towards the lib/cmake folder of the Qt installation you want to use.
 # Comment the line to use the default system libs.
-export ISCORE_QT_PATH="$HOME/Qt/5.3/clang_64/"
+# export ISCORE_QT_PATH="$HOME/Qt/5.3/clang_64/"
